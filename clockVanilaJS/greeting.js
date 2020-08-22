46f9c11339bd9e7e67596ce4b5059fd18e4877a2
Jihyun Lim <69613656+thirco@users.noreply.github.com>
2020-08-22 23:55:07 +0900

    Add files via upload

---
+++ clockVanilaJS/greeting.js
@@ -0,0 +1,44 @@
+const form = document.querySelector(".js-form"),
+    input = form.querySelector("input"),
+    greeting = document.querySelector(".js-greetings");
+
+const USER_LS = "currentUser",
+    showing_CN = "showing";
+
+function saveName(text) {
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue)
+}
+function askForName() {
+    form.classList.add(showing_CN);
+    form.addEventListener("submit", handleSubmit)
+};
+
+function paintGreeting(text) {
+    form.classList.remove(showing_CN);
+    greeting.classList.add(showing_CN);
+    greeting.innerText = `Hello ${text} ♥`;
+}
+
+function loadName() {
+    const currentUser = localStorage.getItem(USER_LS);
+    if (currentUser === null) {
+        // user is not
+        askForName();
+    } else {
+        // user is
+        paintGreeting(currentUser);
+    }
+}
+
+function init() {
+    loadName();
+}
+
+init();
